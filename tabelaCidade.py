271269532c4e0d11d7b2251c23178bcfb4691d9d
gondo321 <48776175+gondo321@users.noreply.github.com>
2019-11-19 17:51:46 -0300

    Add files via upload

---
+++ tabelaCidade.py
@@ -0,0 +1,51 @@
+#Importar biblioteoca
+import sqlite3
+
+
+# Criação da tabela cidade
+def tabela_cidade(conexao):
+    cursor = conexao.cursor()
+
+    sql = '''
+        CREATE TABLE IF NOT EXISTS cidade,
+        nome TEXT NOT NULL,
+        idest INTEGER NOT NULL,
+        FOREIGN KEY(idest) REFERENCES estado(rowid)
+        );'''
+
+    cursor.execute(sql)
+
+
+#Inserir dados na tabela cidade
+def Inserir_dados(conexao):
+    nome = input("Nome da cidade: ")
+    
+
+    cursor = conexao.cursor()
+
+    sql = '''
+        INSERT INTO cidade VALUES(
+            '{}'
+        );
+        '''.format(nome)
+    cursor.execute(sql)
+
+    #commit para salvar
+    conexao.commit()
+
+
+def lista(conexao):
+    cursor = conexao.cursor()
+
+    sql = '''
+        SELECT rowid, * FROM cidade;
+    '''
+
+    cursor.execute(sql)
+    lista_cidade = cursor.fetchall()
+    for c in lista_cidade:
+        print('''
+        ID:{}
+        NOME:{}
+        idest: {}
+        ''').format(c[0],c[1],c[2])
